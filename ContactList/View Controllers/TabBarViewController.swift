d96fc6669b5746989aa8179225cf3d5d1a3be869
Dmitry <ecawd>
2021-12-29 14:43:25 +0300

    v2.5

--- ContactList/View Controllers/TabBarViewController.swift
+++ ContactList/View Controllers/TabBarViewController.swift
@@ -15,20 +15,6 @@ class TabBarViewController: UITabBarController {
     
     private let personsModel = Person.getContactList()
 
-    // MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let tabBarControllers = segue.destination as? UITabBarController else { return }
-//        guard let viewControllers = tabBarControllers.viewControllers else { return }
-//
-//        for viewController in viewControllers {
-//            if let contactVC = viewController as? ContactListViewController {
-//                contactVC.infoList = personsModel
-//            } else if let personVC = viewController as? ListPersonViewController {
-//                personVC.persons = personsModel
-//            }
-//        }
-    }
 }
 
 // MARK: - ModelDataSource
